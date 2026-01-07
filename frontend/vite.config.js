79591256eee4a310e1ae9ccbb33dad6492e24735
mkishore <mkishore@speshway.com>
2026-01-07 21:30:36 +0530

    vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import envCompatible from 'vite-plugin-env-compatible';
+import EnvironmentPlugin from 'vite-plugin-environment';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    envCompatible(),
+    EnvironmentPlugin('all', { prefix: 'REACT_APP_' }),
+  ],
+  define: {
+    'process.env': {},
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+});
